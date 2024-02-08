d5d6df8d196d0d048e7934e50fa1d47687638a26
ninthIvy <136642053+ninthIvy@users.noreply.github.com>
2024-02-07 20:14:53 -0800

    Update abaddon.sh
    TIL zypak is only for electron

--- abaddon.sh
+++ abaddon.sh
@@ -1,1 +1,2 @@
-env TMPDIR="XDG_RUNTIME_DIR/app/${FLATPAK_ID:-io.github.uowuo.abaddon}" zypak-wrapper /app/abaddon/abaddon
+env TMPDIR="XDG_RUNTIME_DIR/app/${FLATPAK_ID:-io.github.uowuo.abaddon}" /app/abaddon/abaddon
+
